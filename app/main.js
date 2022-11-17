72eb55a72d1e7fadea9d4140ca563c20edb0f7f8
senlin <gougewh@sina.com>
2022-11-18 00:53:27 +0800

    调试windows打包

--- app/main.js
+++ app/main.js
@@ -1,12 +1,19 @@
 const {app, BrowserWindow } = require('electron')
 const path = require('path')
+const isDev = require('electron-is-dev')
+
 app.on('ready', async () => {
   const win = new BrowserWindow({
     width: 400,
     height: 600,
     webPreferences: { 
       nodeIntegration: true,
+      webSecurity: false
     },
   })
-  win.loadURL(`file://${path.resolve(__dirname, './_dist/index.html')}`)
+  if (isDev) {
+    win.loadURL('http://localhost:5500')
+  } else {
+    win.loadURL('file://' + path.resolve(__dirname, '../_dist/index.html'))
+  }
 })
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { render } from 'preact'
 import { App } from './app'
-import './index.css'
+import './assets/style/reset.css'
+import './assets/style/index.css'
 
 render(<App />, document.getElementById('app'))
