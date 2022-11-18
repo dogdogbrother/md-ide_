893be45557071a71ad0d7232ad1dc3d274222d1f
森林 <gougewh@sina.com>
2022-11-18 14:11:57 +0800

    添加prismjs

--- pages/edit/app.jsx
+++ pages/edit/app.jsx
@@ -1,11 +1,50 @@
-import { useState } from 'preact/hooks'
-
+import { useState, useRef, useEffect } from 'preact/hooks'
+import Prism from 'prismjs'
+import './index.css'
 export function App() {
+  const [content, setContent] = useState('')
+  const textareaRef = useRef(null)
+  const codeRef = useRef(null)
+  useEffect(() => {
+    if (codeRef && codeRef.current) {
+      // Prism.highlightElement(codeRef.current)
+    }
+  }, [content])
+  function onInput(e) {
+    setContent(e.target.value)
+  }
+  function onEnterKey(e) {
+    const pointStart = e.target.selectionStart
+    const pointEnd = e.target.selectionEnd
+    const textLength = e.target.value.length
+    const content = e.target.value
+    if (e.key === 'Tab') {
+      if (textLength === pointStart) {
+        const newContent = content + '  '
+        setContent(newContent)
+        textareaRef.current.value = newContent
+        return e.preventDefault()
+      } else {
+        const newContent = content.slice(0, pointStart) + '  ' + content.slice(pointEnd)
+        e.target.setSelectionRange(pointStart + 2, pointStart + 2)
+        textareaRef.current.value = newContent
+        setContent(newContent)
+        return e.preventDefault()
+      }
+    }
+  }
   return (
-    <>
-      <div>
-        1111111111111111
-      </div>
-    </>
+    <div className='edit-wrap'>
+      <textarea 
+        ref={textareaRef}
+        onInput={onInput}
+        onKeyDown={onEnterKey}
+      />
+      <pre>
+        <code ref={codeRef} className="language-markdown line-numbers">
+          {content}
+        </code>
+      </pre>
+    </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import path from 'path'
+import { prismjsPlugin } from 'vite-plugin-prismjs'
 
 export default defineConfig({
   base: './',
@@ -9,7 +10,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src')
     }
   },
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    prismjsPlugin({
+      theme: "dark", //主题名称
+      languages: 'all',
+      css: true,
+      // plugins: ["line-numbers", "show-language"]
+      plugins: ["show-language"]
+    }),
+  ],
   build: {
     outDir: '_dist',
     rollupOptions: {
