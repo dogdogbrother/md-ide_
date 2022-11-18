d6f8395ef1cb656918b4ff85e0bb1787a3d8e9f9
森林 <gougewh@sina.com>
2022-11-18 14:32:25 +0800

    资源位置调整

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
-import '../../assets/style/reset.css'
-import '../../assets/style/index.css'
+import './assets/style/reset.css'
+import './assets/style/index.css'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <App />
