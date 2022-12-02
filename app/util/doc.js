ea9943c24ab03098194a12cabf913191bd63c71f
森林 <gougewh@sina.com>
2022-12-02 17:08:29 +0800

    :bug:

--- app/util/doc.js
+++ app/util/doc.js
@@ -1,20 +1,24 @@
 const { getConfig } = require('./appPath')
 const fs = require('fs')
 const { v4 } = require('uuid')
-const { md_file } = getConfig()
-const docPath = md_file + '/docs'
+
 function getDocs() {
+  const { md_file } = getConfig()
+  const docPath = md_file + '/docs'
   const docsDir = fs.readdirSync(docPath)
   return docsDir.filter(file => file.includes('.md')).map(getDocFn)
 }
 
 function getDir() {
+  const { md_file } = getConfig()
+  const docPath = md_file + '/docs'
   const docsDir = fs.readdirSync(docPath)
   // 暂时判定 没有 .md 的都是文件夹  并且排除img文件夹
   return docsDir.filter(file => !file.includes('.md') && file !== 'img')
 }
 function getAllDoc() {
   const dir = []
+  const { md_file } = getConfig()
   getDir().forEach(item => {
     const fullPath = md_file + '/docs' + '/' + item
     const stats = fs.statSync(fullPath)
@@ -41,6 +45,8 @@ const getDocFn = file => {
 
 // 把插入的图片 复制大盘 dpc
 function setDocImg(imgPath) {
+  const { md_file } = getConfig()
+  const docPath = md_file + '/docs'
   const split = imgPath[0].split('.')
   const extensions = split[split.length - 1]
   const _img_path = `/img/${v4()}.${extensions}`
